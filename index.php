f3fc5f075ae9a5419c647f3a45540d7c82ed4d72
mohamedslima <hammaslima@gmail.com>
2022-06-09 22:13:32 +0200

    completed header and styling

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google</title>
+    <!-- link css -->
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="header-top">
+            <!-- logo -->
+            <div class="header-logo">
+                <img src="img/logo.jpeg" alt="logo">
+            </div>
+            <!-- user -->
+            <div class="header-user">
+                <img src="img/user.jpeg" alt="user">
+            </div>
+        </div>
+        <nav>
+            <!-- menu -->
+            <div>
+                <ul class="header-menu">
+                    <li>Introduzione</li>
+                    <li>Norme sulla Privacy</li>
+                    <li>Termini di servizio</li>
+                    <li>Tecnologie</li>
+                    <li>Domande Frequenti</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
